ad697574886181a71f2ef2fac340165b344c29d8
Thapelo Malulyck <t.malulyck@gmail.com>
2022-06-07 20:42:17 +0200

    case checker

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: load a character to check
+ * Return: 1 if lowercase, 0 otherwise if Capital
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
