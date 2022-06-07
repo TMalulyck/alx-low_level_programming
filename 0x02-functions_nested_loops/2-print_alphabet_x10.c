9492ffc1896c62761e35c50cc6734006c8d6cd4c
Thapelo Malulyck <t.malulyck@gmail.com>
2022-06-07 20:07:56 +0200

    try again

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_X10 - print the alphabet,ten times
- * followed by a new line
+ * print_alphabet_X10 - print the alphabet,ten times ,followed by a new line
  */
 void print_alphabet_x10(void)
 {
