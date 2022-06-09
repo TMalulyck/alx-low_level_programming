c2e31ca125c8f59261e9d5d6d9da8fa30e963f26
Thapelo Malulyck <t.malulyck@gmail.com>
2022-06-09 17:06:19 +0200

    Fixed

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - function that prints the numbers, 
- * from 0 to 9, 
+ * print_numbers - function that prints the numbers, from 0 to 9
  * followed by a new line
  */
 
